b5d9c9d4927cc4c3fe09bd44eca9b3ce954aeb6c
Silver·湫曗 <autumnsilverleaves@gmail.com>
2019-05-27 14:18:16 +0800

    独立出ts、stylus到src目录

--- gulpfile.ts
+++ gulpfile.ts
@@ -9,48 +9,55 @@ import * as postcss from "gulp-postcss"
 import * as ts from "gulp-typescript"
 import { FSWatcher } from "fs";
 
-
 let tsProject = ts.createProject("tsconfig.json");
 
-function acss(done) {
-    gulp.src(`pages/**/*.styl`)
+const dist = "pages"
+
+function acss() {
+    return gulp.src(`src/**/*.styl`)
         .pipe(plumber())
         .pipe(stylus())
         .pipe(postcss([autoprefixer()]))
         .pipe(rename({
             extname: '.acss',
         }))
-        .pipe(gulp.dest("pages"))
-        .on("end", () => {
-            done();
-        })
+        .pipe(gulp.dest(dist))
 }
 
-function tsc(done) {
-    tsProject.src()
-        .pipe(plumber())
+function tsc() {
+    return tsProject.src()
         .pipe(tsProject())
         .js
-        .pipe(gulp.dest("."))
-        .on("end", () => {
-            done();
-        })
+        .pipe(gulp.dest(dist))
+}
+
+function json() {
+    return gulp.src(`src/**/*.json`)
+        .pipe(gulp.dest(dist));
+}
+
+function axml() {
+    return gulp.src(`src/**/*.axml`)
+        .pipe(gulp.dest(dist));
 }
 
 console.log("starting gulp task...")
-gulp.task("default", gulp.parallel(acss, tsc))
+gulp.task("default", gulp.parallel(acss, tsc, json, axml))
 
 console.log("starting watching...")
 
 let watcher = <FSWatcher[]>[]
 
-watcher.push(gulp.watch(`./**/*.styl`, acss))
-watcher.push(gulp.watch(`./**/*.ts`, tsc))
+watcher.push(gulp.watch(`src/**/*.styl`, acss))
+watcher.push(gulp.watch(`src/**/*.ts`, tsc))
+watcher.push(gulp.watch(`src/**/*.json`, json))
+watcher.push(gulp.watch(`src/**/*.axml`, axml))
 
 watcher.forEach((ele) => {
     ele.on("change", (fileName, eventType) => {
         console.log(`${fileName} changed...`)
     })
+
     ele.on("error", (error) => {
         console.error(error)
     })
