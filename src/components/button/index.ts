d44a341cc6afb2cdb7a335151332cacf45cc7f2e
Silver·湫曗 <autumnsilverleaves@gmail.com>
2019-06-17 11:20:22 +0800

    按钮功能实现

---
+++ src/components/button/index.ts
@@ -0,0 +1,50 @@
+Component({
+    mixins: [],
+    data: function () {
+        return {
+            loading: false,
+            disabled: false
+        }
+    },
+    props: {
+        async onTap() {
+
+            console.log("按下按钮")
+
+            return new Promise(function (resolve, reject) {
+                setTimeout(function () {
+                    resolve('foo');
+                }, 3000);
+            })
+        },
+        text: "按钮",
+    },
+    didMount() {
+
+    },
+    didUpdate() {
+
+    },
+    didUnmount() {
+
+    }
+    ,
+    methods: {
+        async taptap() {
+            try {
+                this.setData({
+                    loading: true,
+                    disabled: true
+                })
+                await this.props.onTap()
+            } catch (error) {
+                console.log(error)
+            } finally {
+                this.setData({
+                    loading: false,
+                    disabled: false
+                })
+            }
+        }
+    }
+})
