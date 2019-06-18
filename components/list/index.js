4f457e3292d5f21444145d43bf83a457bf68cfa5
Silver·湫曗 <autumnsilverleaves@gmail.com>
2019-06-18 13:55:38 +0800

    添加grid和list组件模板

---
+++ components/list/index.js
@@ -0,0 +1,18 @@
+Component({
+    mixins: [],
+    data: function () {
+        return {};
+    },
+    props: {
+        onChange() {
+            console.log(this.data);
+        },
+    },
+    didMount() {
+    },
+    didUpdate() {
+    },
+    didUnmount() {
+    },
+    methods: {}
+});
---
+++ src/components/grid/index.ts
@@ -0,0 +1,25 @@
+Component({
+    mixins: [],
+    data: function () {
+        return {
+        }
+    },
+    props: {
+        onChange() {
+            console.log(this.data)
+        },
+    },
+    didMount() {
+      
+    },
+    didUpdate() {
+        
+    },
+    didUnmount() {
+
+    }
+    ,
+    methods: {
+
+    }
+})
