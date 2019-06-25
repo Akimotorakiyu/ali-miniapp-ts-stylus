d7321049d964e4b89022fd0f7481ad6b5ba18e20
Silver·湫曗 <autumnsilverleaves@gmail.com>
2019-06-25 12:45:41 +0800

    默认关闭下拉

--- dist/components/tabbar/index.js
+++ dist/components/tabbar/index.js
@@ -5,9 +5,7 @@ Component({
         return {};
     },
     props: {
-        onChange() {
-            console.log(this.data);
-        },
+        onItemClick: undefined,
         items: [],
         accessControl: false,
         access: {},
@@ -20,5 +18,19 @@ Component({
     },
     didUnmount() {
     },
-    methods: {}
+    methods: {
+        onItemClick(event) {
+            if (this.props.onItemClick) {
+                this.props.onItemClick(event.target.dataset);
+            }
+            else {
+                my.navigateTo({
+                    url: this.props.items[event.target.dataset.index].path,
+                    fail: () => {
+                        console.error("跳转失败");
+                    }
+                });
+            }
+        }
+    }
 });
