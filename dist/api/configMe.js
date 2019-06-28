7b481371854f0f2a173b4e6494e39636362c4c45
Silver·湫曗 <autumnsilverleaves@gmail.com>
2019-06-28 14:56:43 +0800

    添加configMe.ts文件

---
+++ dist/api/configMe.js
@@ -0,0 +1,10 @@
+let config = {
+    dev: {
+        baseUrl: "https://eqm.pinza.ltd:1443/EQM",
+    },
+    pro: {
+        baseUrl: "http://192.168.1.300/oss",
+    }
+};
+let myConf = config.dev;
+export default myConf;
